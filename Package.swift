e11e2d6fc8f70c2ff3c5e4a606f5df37eba3bedc
skinxcheg <skinxcheg>
2022-03-28 21:25:11 +0300

    11

--- Package.swift
+++ Package.swift
@@ -14,15 +14,15 @@ let package = Package(
             targets: ["TVOSPackage"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.5.0"))
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "TVOSPackage",
-            dependencies: []),
+            dependencies: ["Alamofire"]),
         .testTarget(
             name: "TVOSPackageTests",
             dependencies: ["TVOSPackage"]),
--- Sources/TVOSPackage/TVOSPackage.swift
+++ Sources/TVOSPackage/TVOSPackage.swift
@@ -1,9 +1,15 @@
 import SwiftUI
+import Alamofire
 
 @available(iOS 14, macOS 11.0, tvOS 13.0, *)
 public struct TVOSPackage : View {
     public init() {}
     public var body: some View {
         Text("Package Text")
+            .onTapGesture {
+                AF.request("https://httpbin.org/get").response { response in
+                    debugPrint(response)
+                }
+            }
     }
 }
