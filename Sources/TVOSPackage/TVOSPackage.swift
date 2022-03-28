166a0b61c3e64c66a02a535911d7e08ee4327b4a
skinxcheg <skinxcheg>
2022-03-28 20:53:09 +0300

    second

--- Sources/TVOSPackage/TVOSPackage.swift
+++ Sources/TVOSPackage/TVOSPackage.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 @available(iOS 14, macOS 11.0, tvOS 13.0, *)
 public struct TVOSPackage : View {
+    public init() {}
     public var body: some View {
         Text("Package Text")
     }
